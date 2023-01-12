23ddb6e53e83d2a62f00c1640ad7f7d5c8dd895e
iwan <1322677864@qq.com>
2023-01-12 18:18:59 +0800

    fix

--- src/composeables/game/scene/game.ts
+++ src/composeables/game/scene/game.ts
@@ -1,8 +1,8 @@
 import Phaser from "phaser";
-
+import { Player } from "./player";
 
 export default class Scene extends Phaser.Scene {
-
+    player;
   constructor() {
     super("GameScene");
   }
@@ -14,6 +14,8 @@ export default class Scene extends Phaser.Scene {
     this.load.image("ground", "/game/platform.png");
     // 加载星星
     this.load.image("star", "/game/star.png");
+    // 加载人物
+    this.load.spritesheet('player', '/game/dude.png', { frameWidth: 32, frameHeight: 48 });
 
   }
 
@@ -21,13 +23,39 @@ export default class Scene extends Phaser.Scene {
     console.log("创建场景");
     // 添加背景
     this.add.image(400, 300, "sky").setScale(2, 1);
-    this.add.image(400, 300, 'star');
+    this.add.image(400, 300, "star");
     const platforms = this.physics.add.staticGroup();
-    platforms.create(400, 500, 'ground').setScale(10).refreshBody();
-    // platforms.create(600, 400, 'ground');
-    platforms.create(50, 250, 'ground');
-    platforms.create(1050, 220, 'ground');
-    console.log(platforms);
+    platforms.create(400, 400, "ground").setScale(10,1).refreshBody();
+    platforms.create(500, 150, "ground");
+    platforms.create(1050, 220, "ground");
+    platforms.create(50, 250, "ground");
+
+
+    // 创建人物
+    this.player = this.physics.add.sprite(100, 300, "player");
+    this.player.setBounce(0.2);
+    this.player.setCollideWorldBounds(true);
+    this.anims.create({
+      key: "left",
+      frames: this.anims.generateFrameNumbers("player", { start: 0, end: 3 }),
+      frameRate: 10,
+      repeat: -1,
+    });
+    this.anims.create({
+        key: 'turn',
+        frames: [ { key: 'player', frame: 1 } ],
+        frameRate: 20
+    });
+
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('player', { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.physics.add.collider(this.player, platforms);
+
     // 添加地面
     //    console.log(logo)
     //     this.tweens.add({
---
+++ src/composeables/game/scene/player.ts
@@ -0,0 +1,5 @@
+export class Player {
+  constructor(player) {
+    this.player = player;
+  }
+}
